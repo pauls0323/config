e6909c1272a5bfd4ebd45fe774f51611ce7b94cf
pauls0323 <pauls0323@163.com>
2020-06-05 10:36:33 +0800

    fix ros

--- install/ros.bash
+++ install/ros.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 # sudo apt-get update 
+sudo sh -c '. /etc/lsb-release && echo "deb http://mirrors.ustc.edu.cn/ros/ubuntu/ `lsb_release -cs` main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-get update
 sudo apt-get install -y ros-kinetic-desktop-full \
 ros-kinetic-moveit \
 ros-kinetic-moveit-visual-tools \
